e6251a97ba89124bf21ee6d8516442f815a864e3
KarlRoseus <kroseus3@gmail.com>
2025-08-06 15:30:36 -0400

    Update gender query.sql

--- sql_queries/gender query.sql
+++ sql_queries/gender query.sql
@@ -1,1 +1,9 @@
-
+USE telcoCust;
+-- Comparing churn rate by gender
+SELECT Gender,
+       COUNT(*) AS total_customers,
+       ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM telco_customers), 2) AS percent_of_total,
+       SUM(Churn_Label = 'Yes') AS churned_customers,
+       ROUND(AVG(Churn_Label = 'Yes') * 100, 2) AS churn_rate_percentage
+FROM telco_customers
+GROUP BY Gender;
