c13c5e488ad70405e406bf074ed03db8b5860184
KarlRoseus <kroseus3@gmail.com>
2025-08-06 16:12:07 -0400

    Update Create_Table.sql

--- sql/Create_Table.sql
+++ sql/Create_Table.sql
@@ -38,22 +38,3 @@ CREATE TABLE telco_customers (
   churn_category VARCHAR(50),
   churn_reason VARCHAR(100)
 );
-
-SET GLOBAL local_infile = 1;
-
-LOAD DATA LOCAL INFILE 'C:/Users/krose/Downloads/telcoCust.csv'
-INTO TABLE telco_customers
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS;
-
-SELECT COUNT(*) FROM telco_customers;
-SELECT * FROM telco_customers LIMIT 10;
-
-
-
-
-
-
-
